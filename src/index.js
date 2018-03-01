3f41d0078f61350c8bb8d7cc7d4a354432b9b546
dimanadko <dimanadobko@gmail.com>
2018-03-01 23:32:36 +0200

    Final commit

--- src/index.js
+++ src/index.js
@@ -15,20 +15,26 @@ for (const data of arrData) {
     const ride = rides.shift();
     const routeLen = dist(ride.start, ride.finish);
 
+    let minCar = 0;
+    let minTime = data.end;
     for (let k = 0; k < data.vehicles.length; k++) {
       const c = cars[k];
       const toSP = dist(c, ride.start);
-      const toST = ((ride.start.time - c.freeTime + toSP) > 0) ?
-        (ride.start.time - c.freeTime + toSP) :
+      const toST = ((ride.start.time - (c.freeTime + toSP)) > 0) ?
+        (ride.start.time - (c.freeTime + toSP)) + c.freeTime + toSP :
         (c.freeTime + toSP);
       const full = toST + routeLen;
       if (full > ride.finish.time || full > data.end) continue;
-      c.routesToDrive.push(ride);
-      c.x = ride.finish.x;
-      c.y = ride.finish.y;
-      c.freeTime += full;
-      break;
+      if (full < minTime) {
+        minTime = full;
+        minCar = k;
+      }
     }
+    cars[minCar].routesToDrive.push(ride);
+    cars[minCar].x = ride.finish.x;
+    cars[minCar].y = ride.finish.y;
+    cars[minCar].freeTime = minTime;
+    cars.sort((car1, car2) => (car1.freeTime > car2.freeTime) ? -1 : 1);
   }
 
   writeData(cars, data.fileName);
