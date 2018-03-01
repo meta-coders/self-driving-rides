488bd5d51a0bb0b42c95de043d56589f003509ca
o-rumiantsev <alex.rum12.12.12@gmail.com>
2018-03-01 22:39:28 +0200

    Try2

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
 'use strict';
 
-const { readData, dist } = require('./inputParser');
+const { readData, dist, writeData } = require('./inputParser');
 
-const data = readData()[1];
-const cars = data.vehicles;
-const rides = data.routes;
+const arrData = readData();
 
-const sort = (a, b) => b.start.time - a.start.time;
-rides.sort(sort);
+for (const data in arrData) {
+  const cars = data.vehicles;
+  const rides = data.routes;
 
-while (rides.length > 0) {
-  const ride = rides.shift();
-  const routeLen = dist(ride.start, ride.finish);
+  const sort = (a, b) => b.start.time - a.start.time;
+  rides.sort(sort);
 
-  for (let k = 0; k < data.vehicles.length; k++) {
-    const c = cars[k];
-    const toSP = dist(c, ride.start);
-    const toST = ((ride.start.time - c.freeTime + toSP) > 0) ?
-      (ride.start.time - c.freeTime + toSP) :
-      (c.freeTime + toSP);
-    const full = toST + routeLen;
-    if (full > ride.finish.time || full > data.end) continue;
-    c.routesToDrive.push(ride);
-    c.x = ride.finish.x;
-    c.y = ride.finish.y;
-    c.freeTime += full;
-    break;
+  while (rides.length > 0) {
+    const ride = rides.shift();
+    const routeLen = dist(ride.start, ride.finish);
+
+    for (let k = 0; k < data.vehicles.length; k++) {
+      const c = cars[k];
+      const toSP = dist(c, ride.start);
+      const toST = ((ride.start.time - c.freeTime + toSP) > 0) ?
+        (ride.start.time - c.freeTime + toSP) :
+        (c.freeTime + toSP);
+      const full = toST + routeLen;
+      if (full > ride.finish.time || full > data.end) continue;
+      c.routesToDrive.push(ride);
+      c.x = ride.finish.x;
+      c.y = ride.finish.y;
+      c.freeTime += full;
+      break;
+    }
   }
-}
 
-console.dir(cars);
+  writeData(cars);
+}
